4d303c72847bbabf271175939e53565d28a45f4d
Keiter0309 <vuongtuankiet33@gmail.com>
2026-05-11 13:58:12 +0700

    feat: add staging

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+
+type Env = 'local' | 'staging' | 'production'
+
+const env = (process.env.APP_ENV as Env) ?? 'local'
+
+const urlByEnv: Record<Env, string> = {
+  local: process.env.DATABASE_URL_LOCAL!,
+  staging: process.env.DATABASE_URL_STG!,
+  production: process.env.DATABASE_URL_PROD!,
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
--- user-data.sh
+++ user-data.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-apt-get update -y
-apt-get upgrade -y
+OS_TYPE=$(grep ^ID= /etc/os-release | cut -d'=' -f2 | sed 's/"//g')
 
-apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+if [ "$OS_TYPE" == "ubuntu" ]; then
+    apt-get update -y
+    apt-get install -y ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update -y
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    usermod -aG docker ubuntu
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Docker Engine
-apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Docker Compose v2
-mkdir -p /usr/local/lib/docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-
-
-usermod -aG docker ubuntu
+elif [[ "$OS_TYPE" == "amzn" || "$OS_TYPE" == "al2023" ]]; then
+    dnf update -y
+    dnf install -y docker
+    usermod -aG docker ec2-user
+fi
 
 systemctl enable docker
 systemctl start docker
